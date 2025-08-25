7a45d9c014ee20033813b89460b51076912cd3fa
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-08-25 19:13:29 +0800

    author api

--- controllers/authorcontroller.go
+++ controllers/authorcontroller.go
@@ -1,4 +1,4 @@
-package authcontroller
+package controllers
 
 import (
 	"encoding/json"
@@ -14,7 +14,7 @@ import (
 )
 
 // Get All
-func Index(w http.ResponseWriter, r *http.Request) {
+func IndexAuthor(w http.ResponseWriter, r *http.Request) {
 	var author []models.Author
 
 	if err := config.DB.Find(&author).Error; err != nil {
@@ -26,7 +26,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 // Create
-func Create(w http.ResponseWriter, r *http.Request) {
+func CreateAuthor(w http.ResponseWriter, r *http.Request) {
 	var author models.Author
 	var existingEmail models.Author
 	var existingName models.Author
@@ -72,11 +72,11 @@ func Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	helper.Response(w, 200, "success created author", author)
+	helper.Response(w, 201, "success created author", author)
 }
 
 // Detail
-func Detail(w http.ResponseWriter, r *http.Request) {
+func DetailAuthor(w http.ResponseWriter, r *http.Request) {
 	idParams := mux.Vars(r)["id"]
 
 	id, _ := strconv.Atoi(idParams)
@@ -96,21 +96,87 @@ func Detail(w http.ResponseWriter, r *http.Request) {
 	helper.Response(w, 200, "detail author", author)
 }
 
-func Update(w http.ResponseWriter, r *http.Request) {
+// Update
+func UpdateAuthor(w http.ResponseWriter, r *http.Request) {
+	idParams := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParams)
+
+	var author models.Author
+	if err := config.DB.First(&author, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helper.Response(w, 404, "author not found", nil)
+			return
+		}
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	var input models.Author
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		helper.Response(w, 400, "invalid request body", nil)
+		return
+	}
+	defer r.Body.Close()
+
+	if input.Name == "" {
+		helper.Response(w, 400, "name is required", nil)
+		return
+	}
+	if input.Email == "" {
+		helper.Response(w, 400, "email is required", nil)
+		return
+	}
+	if input.Age <= 0 {
+		helper.Response(w, 400, "age is required and must be greater than 0", nil)
+		return
+	}
+	if input.Gender == "" {
+		helper.Response(w, 400, "gender is required", nil)
+		return
+	}
+
+	var existingName models.Author
+	if err := config.DB.Where("name = ? AND id != ?", input.Name, id).First(&existingName).Error; err == nil {
+		helper.Response(w, 400, "name already exists", nil)
+		return
+	}
+
+	var existingEmail models.Author
+	if err := config.DB.Where("email = ? AND id != ?", input.Email, id).First(&existingEmail).Error; err == nil {
+		helper.Response(w, 400, "email already exists", nil)
+		return
+	}
+
+	author.Name = input.Name
+	author.Email = input.Email
+	author.Age = input.Age
+	author.Gender = input.Gender
+
+	if err := config.DB.Save(&author).Error; err != nil {
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	helper.Response(w, 200, "success updated author", nil)
+}
+
+// Delete
+func DeleteAuthor(w http.ResponseWriter, r *http.Request) {
 	idParams := mux.Vars(r)["id"]
 
 	id, _ := strconv.Atoi(idParams)
 
 	var author models.Author
 
-	if err := config.DB.First(&author, id).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			helper.Response(w, 404, "author not found", nil)
-			return
-		}
+	res := config.DB.Delete(&author, id)
 
-		helper.Response(w, 500, err.Error(), nil)
-		return
+	if res.Error != nil {
+		helper.Response(w, 500, res.Error.Error(), nil)
 	}
 
+	if res.RowsAffected == 0 {
+		helper.Response(w, 404, "author not found", nil)
+	}
+
+	helper.Response(w, 200, "success deleted author", nil)
 }
--- routes/author.go
+++ routes/author.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	authcontroller "golang-restApi/controllers"
+	"golang-restApi/controllers"
 
 	"github.com/gorilla/mux"
 )
@@ -9,8 +9,9 @@ import (
 func AuthorRoutes(r *mux.Router) {
 	router := r.PathPrefix("/authors").Subrouter()
 
-	router.HandleFunc("", authcontroller.Index).Methods("GET")
-	router.HandleFunc("", authcontroller.Create).Methods("POST")
-	router.HandleFunc("/{id}", authcontroller.Detail).Methods("GET")
-	router.HandleFunc("/{id}", authcontroller.Update).Methods("PUT")
+	router.HandleFunc("", controllers.IndexAuthor).Methods("GET")
+	router.HandleFunc("", controllers.CreateAuthor).Methods("POST")
+	router.HandleFunc("/{id}", controllers.DetailAuthor).Methods("GET")
+	router.HandleFunc("/{id}", controllers.UpdateAuthor).Methods("PUT")
+	router.HandleFunc("/{id}", controllers.DeleteAuthor).Methods("DELETE")
 }
