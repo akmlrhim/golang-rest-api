b55b336c62c78fc213ca1c8159955d2a1a92ff43
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-08-26 20:34:12 +0800

    add cors

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"golang-restApi/routes"
 	"net/http"
 
+	"github.com/rs/cors"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/gorilla/mux"
@@ -17,6 +18,15 @@ func main() {
 
 	r := mux.NewRouter()
 	routes.RoutesIndex(r)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+
+	handler := c.Handler(r)
+
 	log.Println("Server running on port", config.ENV.PORT)
-	http.ListenAndServe(fmt.Sprintf(":%v", config.ENV.PORT), r)
+	http.ListenAndServe(fmt.Sprintf(":%v", config.ENV.PORT), handler)
 }
